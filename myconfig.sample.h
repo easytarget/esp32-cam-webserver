2de07c1eae43369eed7e890480018aa83ff8bd60
John Floyd <jfloyd@bigpond.net.au>
2020-04-11 21:16:28 +1000

    Additional myconfig options for rotation and wifi AP

--- myconfig.sample.h
+++ myconfig.sample.h
@@ -3,9 +3,15 @@
 
 const char* ssid = "my-access-point-ssid";
 const char* password = "my-access-point-password";
-
+const int   wifichan  = 5;
 
 // Optionally give the camera a name for the web interface 
 //  (nb: this is not the network hostname)
+#define CAM_NAME "UltiCam"
 
-#define CAM_NAME "my-awesome-webcam"
+// Optional initial rotation 
+#define CAM_ROTATION 90
+
+// Optional AP setup
+#define WIFI_AP
+
