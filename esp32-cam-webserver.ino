5063fbef8dccde18e1f023458e6cef8e74eb4413
Owen <owen@easytarget.org>
2020-09-21 21:59:53 +0200

    Discreet camera option settings defaults, commented out by default

--- esp32-cam-webserver.ino
+++ esp32-cam-webserver.ino
@@ -250,6 +250,33 @@ void setup() {
     s->set_framesize(s, FRAMESIZE_SVGA);
   #endif
 
+  /*
+   * Add any other defaults you want to apply at startup here:
+   * uncomment the line and set the value as desired (see the comments)
+   */
+  //s->set_brightness(s, 0);     // -2 to 2
+  //s->set_contrast(s, 0);       // -2 to 2
+  //s->set_saturation(s, 0);     // -2 to 2
+  //s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  //s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  //s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  //s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  //s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  //s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  //s->set_ae_level(s, 0);       // -2 to 2
+  //s->set_aec_value(s, 300);    // 0 to 1200
+  //s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  //s->set_agc_gain(s, 0);       // 0 to 30
+  //s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  //s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+  //s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+  //s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  //s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+  //s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  //s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  //s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  //s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
+
   // Feedback that hardware init is complete and we are now attempting to connect
   Serial.println("Wifi Initialisation");
   flashLED(400);
