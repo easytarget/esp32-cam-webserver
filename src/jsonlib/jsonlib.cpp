56c52672b2f751fbbc8f3a612683392735039ecd
Owen Carter <owen.carter@gmail.com>
2021-07-01 11:29:41 +0200

    Import latest fixes from upstream jsonlib (#132)
    * Fix an off-by-one error that might affect us, etc.

--- src/jsonlib/jsonlib.cpp
+++ src/jsonlib/jsonlib.cpp
@@ -79,7 +79,7 @@ String jsonExtract(String json, String name){
   if(next == '\"'){
     //Serial.println(".. a string");
     start = start + 1;
-    stop = json.indexOf('"', start + 1);
+    stop = json.indexOf('"', start);
   }
   else if(next == '['){
     //Serial.println(".. a list");
@@ -112,7 +112,7 @@ String jsonExtract(String json, String name){
   else if(next == '.' || next == '-' || ('0' <= next  && next <= '9')){
     //Serial.println(".. a number");
     int i = start;
-    while((i++ < json.length() && json.charAt(i) == '.') || ('0' <= json.charAt(i)  && json.charAt(i) <= '9')){
+    while(i++ < json.length() && (json.charAt(i) == '.' || ('0' <= json.charAt(i)  && json.charAt(i) <= '9'))){
     }
     stop = i;
   }
