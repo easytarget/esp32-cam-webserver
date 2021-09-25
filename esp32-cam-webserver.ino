f6709d0ce89740cd2ecb52ecc1d4fd9089c35244
Owen <owen.carter@gmail.com>
2021-09-25 15:13:29 +0200

    Increase default WiFi Connect

--- esp32-cam-webserver.ino
+++ esp32-cam-webserver.ino
@@ -95,7 +95,7 @@ extern void serialDump();
 #endif
 
 #if !defined(WIFI_WATCHDOG)
-    #define WIFI_WATCHDOG 5000
+    #define WIFI_WATCHDOG 8000
 #endif
 
 // Number of known networks in stationList[]
